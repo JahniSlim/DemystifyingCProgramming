370ca03cd431fcac274685c3927f8608f4378a63
JahniSlim <john.olson3@gmail.com>
2017-05-12 18:58:15 -0600

    Update Assignment10.c
    Moved from wrong location

--- DemystifyingCProjects/Section4/Assignment10.c
+++ DemystifyingCProjects/Section4/Assignment10.c
@@ -1,6 +1,5 @@
 /*  ..\Section4\Assignment10.c
     Write a program to print all prime numbers between 0 and 500.
-
     Last Modified: 5/8/17 John Olson
 */
 
@@ -46,3 +45,28 @@ int main()
 
     return 0;
 }
+
+/*
+    Here is a compact code which I just wrote for you. 
+    
+    #include<stdio.h>
+    int main()
+    {
+	    int i, j;
+	
+	    for(i = 1; i<=500; i++)
+	    {
+		    j = 2;
+		    while(j <= i - 1)
+		    {
+			    if( i%j == 0)
+				    break;
+			    j++;
+		    }
+		    if(i==j)
+			    printf("%8d",j);
+	    }
+ 	    printf("\n");   
+        return 0;
+}
+*/
