3506f0aa5667494e509dffe1c70ed53d4dbd27a8
JahniSlim <john.olson3@gmail.com>
2017-05-12 19:21:49 -0600

    Update Lecture 9_9.c
    I stayed away from using loops, trying to stay in line with the first section.

--- DemystifyingCProjects/Section1/Lecture 9_9.c
+++ DemystifyingCProjects/Section1/Lecture 9_9.c
@@ -1,27 +1,30 @@
+
+/*  ..\Section1\Lecture9_9.c
+    A five digit number is entered by the user through the keyboard. Write a C program to reverse the
+    number and print it.
+
+    Last Modified: 5/12/17 John Olson
+*/
+
+
 #include<stdio.h>
 
 int main()
 {
-    int fiveDigit, d1, d2, d3, d4, d5;
+    int num, d1, d2, d3, d4, d5;
 
     printf("Please enter a five digit number: ");
-    scanf("%d", &fiveDigit);
+    scanf("%d", &num);
 
-    d1 = fiveDigit % 10;
-    d2 = (fiveDigit % 100 - d1) / 10;
-    d3 = (fiveDigit % 1000 - (d2 * 10) - d1) / 100;
-    d4 = (fiveDigit % 10000 - (d3 * 100) - (d2 * 10) - d1) / 1000;
-    d5 = (fiveDigit % 100000 - (d4 * 1000) - (d3 * 100) - (d2 * 10) - d1) / 10000;
+    d1 = num % 10;
+    d2 = num % 100 / 10;
+    d3 = num % 1000 / 100;
+    d4 = num % 10000 / 1000;
+    d5 = num % 100000  / 10000;
 
-    printf("Reverse order = %d%d%d%d%d\n", d1, d2, d3, d4, d5);
+    num = d1 * 10000 + d2 * 1000 + d3 * 100 + d4 * 10 + d5;
+
+    printf("Reverse order is: %d", num);
 
     return 0;
 }
-
-/*
-    Attempt is good but you haven't reached the true objective. You are supposed to reverse the original number and not merely 
-    print the number in the reverse order. 
-    So if a particular varibale say x contains 653 and I print it, it would print 653. After you process it for reversing it, 
-    if I try to access the same variable x it should print 356. 
-    I hope you got it, what I mean.
-    */
