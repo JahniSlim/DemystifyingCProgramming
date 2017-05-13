37604740da5d32a8d410be509062198099cc2fc2
JahniSlim <john.olson3@gmail.com>
2017-05-13 09:35:37 -0600

    Update Lecture9_8.c
    Revised. I'm not sure if this is the simplest way to grab individual digits, but it works and is much cleaner.

--- DemystifyingCProjects/Section1/Lecture9_8.c
+++ DemystifyingCProjects/Section1/Lecture9_8.c
@@ -2,16 +2,16 @@
 
 int main()
 {
-    int fiveDigit, d1, d2, d3, d4, d5, sum;
+    int num, d1, d2, d3, d4, d5, sum;
 
     printf("Please enter a five digit number: ");
-    scanf("%d", &fiveDigit);
+    scanf("%d", &num);
 
-    d1 = fiveDigit % 10;
-    d2 = (fiveDigit % 100 - d1) / 10;
-    d3 = (fiveDigit % 1000 - (d2 * 10) - d1) / 100;
-    d4 = (fiveDigit % 10000 - (d3 * 100) - (d2 * 10) - d1) / 1000;
-    d5 = (fiveDigit % 100000 - (d4 * 1000) - (d3 * 100) - (d2 * 10) - d1) / 10000;
+    d1 = num % 10;
+    d2 = num % 100 / 10;
+    d3 = num % 1000 / 100;
+    d4 = num % 10000 / 1000;
+    d5 = num % 100000 / 10000;
 
     sum = d1 + d2 + d3 + d4 + d5;
 
@@ -20,6 +20,3 @@ int main()
     return 0;
 }
 
-/*
-    Logic is good however if possible make it simple if possible. Give it a try. Hint: Avoid too many tens, and hundreds etc.
-*/
