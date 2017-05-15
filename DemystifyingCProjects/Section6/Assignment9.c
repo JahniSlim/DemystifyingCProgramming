e2cb21b3e01b632bc3fc5c98ef0111f2d612622f
JahniSlim <john.olson3@gmail.com>
2017-05-15 12:53:39 -0600

    Add files via upload

---
+++ DemystifyingCProjects/Section6/Assignment9.c
@@ -0,0 +1,51 @@
+/*  ..\Section6\Assignment9.c
+    Any year is entered though the keyboard. Write a function to print whether the year is a
+    leap year or not.
+
+    Last Modified: 5/15/17 John Olson
+*/
+
+#include<stdio.h>
+int DecideIfLeap(int);
+
+int main()
+{
+    int year, leap;
+
+    printf("This program returns whether an entered year is a leap year or not\n\n");
+
+    while(1)
+    {
+        printf("Please enter a year(or 0 to exit): ");
+        scanf("%d", &year);
+
+        if(year == 0)
+            return 0;
+
+        leap = DecideIfLeap(year);
+
+        if(leap == 1)
+            printf("The year %d is a leap year\n\n", year);
+        else
+            printf("The year %d is not a leap year\n\n", year);
+    }
+}
+
+int DecideIfLeap(int n)
+{
+    if(n % 4 == 0)
+    {
+        if(n % 100 == 0 && n % 400 == 0)
+            return 1;
+        else if(n % 100 == 0)
+            return 0;
+
+        return 1;
+    }
+    else
+        return 0;
+
+
+
+
+}
