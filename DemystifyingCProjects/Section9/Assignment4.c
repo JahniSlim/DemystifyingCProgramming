3f49eab80bc5ab074ad30e57a2048dd1c0548e83
Johnny Olson <john.olson3@gmail.com>
2017-05-24 12:33:45 -0600

    Add files via upload

---
+++ DemystifyingCProjects/Section9/Assignment4.c
@@ -0,0 +1,79 @@
+/*  ..\Section9\Assignment4.c
+    A string containing single word is entered through the keyboard. Write a C program using user
+    defined function to check whether the string is palindrome or not.
+
+    Last Modified: 5/24/17 John Olson
+*/
+
+#include<stdio.h>
+#include<string.h>
+#define STR_LEN 80
+
+void DeleteSpaces(char *);
+int CheckPalindrome(const char a[]);
+
+int main()
+{
+    char phrase[STR_LEN + 1];
+    int result = 0;
+
+    printf("This program checks if a phrase is a palindrome\n\n");
+
+    printf("Please enter a phrase:\n");
+    gets(phrase);
+
+    result = CheckPalindrome(phrase);
+
+    if(result == 1)
+        printf("The phrase is a palindrome\n\n");
+    else
+        printf("The phrase is not a palindrome\n\n");
+
+    return 0;
+}
+
+
+
+
+void DeleteSpaces(char *p)
+{
+    char *p2;
+
+    for(; *p != '\0'; p++)
+    {
+        if(*p == ' ')
+        {
+            for(p2 = p; *p2 != '\0'; p2++)
+            {
+                *p2 = *(p2 + 1);
+            }
+        }
+    }
+}
+
+
+
+
+int CheckPalindrome(const char *s)
+{
+    char s1[STR_LEN + 1];
+    char s2[STR_LEN + 1];
+    char *p1 = s1, *p2 = s2;
+
+    strcpy(s1, s);
+    strlwr(s1);
+
+    DeleteSpaces(s1);
+
+    strcpy(s2, s1);
+    strrev(s2);
+
+    while(*p1 != '\0')
+    {
+        if(*p1++ != *p2++)
+            return 0;
+    }
+
+    return 1;
+}
+
