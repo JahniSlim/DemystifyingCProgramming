27593772f2c3b64a710ebc875ec001326a79a20f
Bhalchandra Gholkar <bgholkar@gmail.com>
2017-06-22 14:49:07 +0530

    Update Assignment7.c
    Study the logic of the modified code

--- DemystifyingCProjects/Section2/Assignment7.c
+++ DemystifyingCProjects/Section2/Assignment7.c
@@ -9,16 +9,27 @@
 int main()
 {
     char ch;
+    int flag = 1;
 
     printf("Please enter any character: ");
     scanf("%c", &ch);
 
-    if (ch >= 33 && ch <= 64)
+    if (ch >= 48 && ch <= 57)
+    {
         printf("\nThe character '%c' is a special character\n", ch);
+        flag = 0;
+    }
     if (ch >= 65 && ch <= 90)
+    {
         printf("\nThe character '%c' is a capital letter\n", ch);
+        flag = 0;
+    }
     if (ch >= 97 && ch <= 122)
+    {
         printf("\nThe character '%c' is a lowercase letter\n", ch);
-
+        flag = 0;
+    }
+    if(flag)
+        printf("The character %c is a special character\n",ch);
     return 0;
 }
