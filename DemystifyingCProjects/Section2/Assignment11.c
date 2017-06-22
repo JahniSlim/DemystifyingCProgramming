1efcceaaf8d97dc40fbdf7826493bbf53e4763b0
Bhalchandra Gholkar <bgholkar@gmail.com>
2017-06-22 14:19:59 +0530

    Update Assignment11.c
    Try this program without using logical and operator inside ternary operator

--- DemystifyingCProjects/Section2/Assignment11.c
+++ DemystifyingCProjects/Section2/Assignment11.c
@@ -1,6 +1,10 @@
 /*  Assignment11.c
     Three integers are input through the keyboard. Write a C program to print largest
     among three numbers. (Use ternary operators)
+    
+    This is one more interesting program wherein I expect to use only ternary operator and not use logical operators. 
+    In C we term it as nesting of ternary operators. The program can be written only for the development of logic. Try it out. Else 
+    I will modify the program
 */
 
 #include<stdio.h>
